56323f35e0ab82fa5ddf0f189aea95e2ec2a063f
patrickteivas <teivas.2011@mail.ru>
2018-04-24 09:39:05 +0300

    Commit

---
+++ Trim/Trim/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage text");
+            var text = Console.ReadLine();
+            text = text.Replace(" ", ""); //Samuti sõnade ja tähtedega
+
+            Console.WriteLine("ALGUS " + text.Trim() + " LÕPP");
+        }
+    }
+}
